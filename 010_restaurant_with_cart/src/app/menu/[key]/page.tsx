80dea172127ee07d8a3f873eb9dd3a1ca11ac64d
stepan.rutz <stepan.rutz@gmx.de>
2023-11-25 22:46:20 +0100

    added 404 page

--- 010_restaurant_with_cart/src/app/menu/[key]/page.tsx
+++ 010_restaurant_with_cart/src/app/menu/[key]/page.tsx
@@ -3,14 +3,22 @@
 
 import { PageContainer } from "@/components/PageContainer";
 import { MenuManager } from "@/data/menumanager"
-import { useParams } from "next/navigation"
+import { useParams, useRouter } from "next/navigation"
 import MenuEntryCard from "../MenuEntryCard"
 import Link from "next/link"
+import { useEffect } from "react"
 
 export default function Page() {
     const { key } = useParams()
 
     const entry = MenuManager.getInstance().entries.find(e => e.key == key)
+    
+    const router = useRouter();
+    useEffect(() => {
+        if (!entry) {
+            router.push('/404')
+        }
+    }, [ entry, router ])
 
     return (
         entry && (
