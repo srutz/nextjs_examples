a3c9c3fe798d87f1d7deef23f56ffd51c18f24e6
stepan.rutz <stepan.rutz@gmx.de>
2023-12-02 11:06:16 +0100

    simple app with simple table component

--- 000_helloworld/src/App.tsx
+++ 000_helloworld/src/App.tsx
@@ -1,13 +1,25 @@
+import {TableView} from "./components/TableView.tsx";
 
 function App() {
 
   return (
-    <>
+    <div className="column-container aligncenter">
         <h1>Simple React App</h1>
-        <div className="grow" style={{ backgroundColor: "red"}}>
+        <div className="grow" >
+            <TableView
+                columns={[
+                    { key: "movie", title: "Film"},
+                    { key: "rating", title: "Note"}
+                ]}
+                data={[
+                    { movie: "Star Wars 1, A new hope", rating: 3 },
+                    { movie: "Star Wars 2, Empire Strikes Back", rating: 1 },
+                    { movie: "Star Wars 3, Return of the Jedi", rating: 4 }
+                ]}
+            ></TableView>
 
         </div>
-    </>
+    </div>
   )
 }
 
---
+++ 000_helloworld/src/components/TableView.tsx
@@ -0,0 +1,37 @@
+
+
+export type TableViewColumn<T> = {
+    key: keyof T & string
+    title: string
+}
+
+export type TableViewProps<T> = {
+    columns: TableViewColumn<T>[]
+    data: T[]
+    width?: string
+}
+
+export function TableView<T>(props: TableViewProps<T>) {
+    const { columns, data, width= "600px" } = props
+    return (
+        <table width={width}>
+            <thead>
+                <tr>
+                    {columns.map(column => (
+                        <th key={column.key}>{column.title}</th>
+                    ))}
+                </tr>
+            </thead>
+            <tbody>
+            {/* Map over the records and create a table row for each record */}
+            {data.map((row, rowindex) => (
+                <tr key={rowindex}>
+                    {columns.map(column => (
+                        <td key={column.key}>{row[column.key] as unknown as any}</td>
+                    ))}
+                </tr>
+            ))}
+            </tbody>
+        </table>
+    )
+}
