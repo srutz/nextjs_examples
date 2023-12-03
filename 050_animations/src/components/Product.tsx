b2811b22e76510203bed738e3c3d7e397ebeb976
stepan.rutz <stepan.rutz@gmx.de>
2023-12-03 21:20:19 +0100

    framermotion

--- 050_animations/src/components/Product.tsx
+++ 050_animations/src/components/Product.tsx
@@ -4,6 +4,7 @@
 import classes from "./Product.module.css"
 import Image from "next/image"
 import { motion } from "framer-motion"
+import {Spacer} from "@/components/Spacer";
 
 export type Product = {
     id: number
@@ -25,6 +26,11 @@ export type ProductProps = {
 
 export function Product(props: ProductProps) {
     const { product } = props
+
+    function formatPrice(n: Number) {
+        return n.toFixed(2)
+    }
+
     return (
         <motion.div
                 initial={{ opacity: 0, scale: 0, y: 150 }}
@@ -36,8 +42,16 @@ export function Product(props: ProductProps) {
                            style={{ width: "280px", height: "auto", minHeight: "140px" }}/>
                 </div>
                 <div className={classes.productbody}>
-                    <div className={classes.producttitle}>{product.title}</div>
-                    <div className={classes.productdescription}>{product.description}</div>
+                    <div className={classes.producttitle} title={product.title}>{product.title}</div>
+                    <div className={classes.productdescription}>
+                        {product.description}
+                        <Spacer grow />
+                        <div className="row-container">
+                            <div className={classes.productstock}>Only {product.stock} left in stock.</div>
+                            <Spacer grow />
+                            <div className={classes.productprice}>{formatPrice(product.price)} €</div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </motion.div>
