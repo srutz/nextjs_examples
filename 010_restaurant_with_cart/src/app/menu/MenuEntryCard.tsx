7b301a84f7e88244cb4ba9c0582c3fec9867f6f1
stepan.rutz <stepan.rutz@gmx.de>
2023-12-06 20:05:40 +0100

    fix add-to-cart button

--- 010_restaurant_with_cart/src/app/menu/MenuEntryCard.tsx
+++ 010_restaurant_with_cart/src/app/menu/MenuEntryCard.tsx
@@ -30,7 +30,7 @@ export default function MenuEntryCard(props: MenuEntryProps) {
     }, [ props.fadeInDelay ])
 
     const cart = useCart()
-    const onClickAdd = (e: MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    const onClickAdd = (e: MouseEvent<HTMLButtonElement,globalThis.MouseEvent>) => {
         cart.addToCart(entry, 1)
         e.preventDefault()
     }
@@ -49,7 +49,7 @@ export default function MenuEntryCard(props: MenuEntryProps) {
             </div>
             <div className={classes["menuentry-description"]}>{entry.description}</div>
             <div className={classes["menuentry-price"]}>Preis: {formatPrice(entry.price)} €
-                <button onClick={(e) => onClickAdd}>Add to cart</button></div>
+                <button onClick={(e) => onClickAdd(e)}>Add to cart</button></div>
         </div>
     )
 }
